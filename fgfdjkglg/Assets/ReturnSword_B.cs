8161fda02478397a3fbb28b6762dd6e3cdf37fee
ost011 <dhkfml@gmail.com>
2021-06-07 23:12:45 +0900

    hiteffecdt, gren instaitat
    sword not

---
+++ fgfdjkglg/Assets/ReturnSword_B.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnSword_B : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cab;
+    private Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //tr = cab.GetComponent<Transform>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("Under"))
+        {
+            //Debug.Log("Sword has fallen!");
+            tr = cab.GetComponent<Transform>();
+            this.gameObject.transform.position = tr.transform.position;
+        }
+    }
+}
